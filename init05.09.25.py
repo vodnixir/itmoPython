b1a9e4bba3d5dab47de37428972095bbb1e0ee67
vodnixir <94857089+vodnixir@users.noreply.github.com>
2025-09-05 11:12:02 +0300

    Update init05.09.25.py

--- init05.09.25.py
+++ init05.09.25.py
@@ -4,13 +4,30 @@ def f(l,n):
     for i in range(len(l)):
         for j in range(i,len(l)):
             if l[i] + l[j] == n and i != j:
-                li.append(i)
-                lj.append(j)
-    return li, lj
+                return li, lj
                         
 l =  [3,5,7,9,3,4,7,8,4,3,5,6,6]
 n = 12
 
-li, lj = f(l,n)
-for i in range(len(li)):
-    print(li[i],lj[i])
+print(f(l,n))
+
+
+
+# def f(l,n):
+#     li = []
+#     lj = []
+#     for i in range(len(l)):
+#         for j in range(i,len(l)):
+#             if l[i] + l[j] == n and i != j:
+#                 li.append(i)
+#                 lj.append(j)
+#     return li, lj
+                        
+# l =  [3,5,7,9,3,4,7,8,4,3,5,6,6]
+# n = 12
+
+# li, lj = f(l,n)
+# for i in range(len(li)):
+#     print(li[i],lj[i])
+
+
