4f1fa2db8a41dfcf83138e3731adedb85cf6a6ed
vodnixir <94857089+vodnixir@users.noreply.github.com>
2025-09-05 12:49:03 +0300

    Update init05.09.25.py

--- init05.09.25.py
+++ init05.09.25.py
@@ -2,7 +2,7 @@ def f(l,n):
     for i in range(len(l)):
         for j in range(i,len(l)):
             if l[i] + l[j] == n and i != j:
-                return li, lj
+                return i, j
                         
 l =  [3,5,7,9,3,4,7,8,4,3,5,6,6]
 n = 12
