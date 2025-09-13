edbc8375f2d1e6fa9c2af6862448d8607e56dac2
vodnixir <94857089+vodnixir@users.noreply.github.com>
2025-09-13 13:25:30 +0300

    Update twosumfunc.py

--- twosumfunc.py
+++ twosumfunc.py
@@ -1,8 +1,12 @@
+
 def ts(l, n):
-    for i in range(len(l)):
-        for j in range(i, len(l)):
-            if l[i] + l[j] == n and i != j:
-                return i, j
+    if len(l)<=1:
+        print('Неподходящий размер массива')
+    else:
+        for i in range(len(l)):
+            for j in range(i, len(l)):
+                if l[i] + l[j] == n and i != j:
+                    return i, j
 
 
 l = [3, 5, 7, 9, 3, 4, 7, 8, 4, 3, 5, 6, 6]
